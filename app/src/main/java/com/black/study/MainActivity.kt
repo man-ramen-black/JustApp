5e845abae75974d22f1c56a9180441ef444653f7
Home <ljh7795@gmail.com>
2019-08-18 22:30:23 +0900

    기능: RxJava로 구구단 개발

--- app/src/main/java/com/black/study/MainActivity.kt
+++ app/src/main/java/com/black/study/MainActivity.kt
@@ -1,26 +1,79 @@
 package com.black.study
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
+import android.text.Editable
+import android.text.TextWatcher
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.functions.BiFunction
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : Activity(), View.OnClickListener, AdapterView.OnItemClickListener {
-    var adapter : MainAdapter? = null;
+@SuppressLint("CheckResult")
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        adapter = MainAdapter()
-        listView.adapter = adapter
-    }
+        setContentView(R.layout.activity_main)
+        simpleInput.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
+                Observable.just(text)
+                    .map { text -> Integer.parseInt("$text") }
+                    .onErrorReturn { 0 }
+                    .flatMap { num ->
+                        Observable.range(1, 9)
+                        .map { dan -> "$num x $dan = ${num*dan}\n"}}
+                    .scan{x, y -> x + y}
+                    .subscribe{result -> simpleResult.text = result}
+            }
+        })
 
-    override fun onClick(view: View?) {
-        when(view?.id){
-            R.id.add -> {}
-        }
-    }
+        /**
+         * BehaviorSubject : 구독하면 최근 항목부터 발행 시작
+         * PublishSubject : 구독하면 구독 시작 후 발행된 항목을 발행 시작 (구독 시작 전 발행된 데이터 소실)
+         * http://reactivex.io/documentation/ko/subject.html
+         */
+        val left = PublishSubject.create<Int>()
+        val right = PublishSubject.create<Int>()
 
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Observable.combineLatest(left, right,
+            BiFunction<Int, Int, String>{leftVal, rightVal ->
+                "$leftVal x $rightVal = ${leftVal*rightVal}"})
+            .onErrorReturn { e -> e.message }
+            .subscribe{result -> mergeResult.text = result}
 
+        leftInput.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
+                var num = 0
+                try {
+                    num = Integer.parseInt("$text")
+                }catch (e : NumberFormatException){
+                    e.printStackTrace()
+                }
+                left.onNext(num)
+            }
+        })
+
+        rightInput.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
+                var num = 0
+                try {
+                    num = Integer.parseInt("$text")
+                }catch (e : NumberFormatException){
+                    e.printStackTrace()
+                }
+                right.onNext(num)
+            }
+        })
     }
 }
