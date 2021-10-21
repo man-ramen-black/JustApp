b96970d14afe191145d52120c81417c4d0380c36
jinhyuk.lee <ljh7795@gmail.com>
2021-10-21 23:00:09 +0900

    BaseActivity 주석 추가

--- app/src/main/java/com/black/study/base/BaseActivity.kt
+++ app/src/main/java/com/black/study/base/BaseActivity.kt
@@ -36,15 +36,33 @@ abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
         Log.d(javaClass.simpleName)
     }
 
+    /**
+     * 현재 액티비티가 최상위가 아닌 경우 호출
+     * ex. 반투명(또는 투명) 액티비티가 위에 노출된 경우 onPause만 호출 (그 외 멀티 윈도우 등등)
+     */
     @CallSuper
     override fun onPause() {
         super.onPause()
         Log.d(javaClass.simpleName)
     }
 
+    /**
+     * 현재 액티비티가 숨겨진 경우 호출
+     * ex. 반투명(또는 투명) 액티비티가 위에 노출된 경우 onPause만 호출 (그 외 멀티 윈도우 등등)
+     */
     @CallSuper
     override fun onStop() {
         super.onStop()
+        Log.d(javaClass.simpleName)
+    }
+
+    /**
+     * targetSdkVersion : Build.VERSION_CODES.P 이상에서는 onStop() 이후에 호출
+     * targetSdkVersion : Build.VERSION_CODES.P 미만인 경우 onStop() 이전에 발생하며 onPause() 이전 또는 이후에 발생할지 여부에 대한 보장은 없습니다.
+     */
+    // https://developer.android.com/reference/android/app/Activity#onSaveInstanceState(android.os.Bundle)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
         Log.d(javaClass.simpleName)
     }
 
