b5b947fca64aee3798a3fba5772b03b9bb47a288
jinhyuk.lee <jinhyuk.lee@netmarble.com>
2026-06-06 15:57:01 +0900

    기타: Compose Navigation 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,6 +103,13 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.fragment.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
     testImplementation(libs.junit)
